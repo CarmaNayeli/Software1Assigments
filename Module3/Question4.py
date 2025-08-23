7bd1ac328632a99f8c0890c8684258056a068cef
CarmaNayeli <caitlyn.c.nayeli@gmail.com>
2025-08-23 18:32:05 +0300

    Create Question4.py

---
+++ Module3/Question4.py
@@ -0,0 +1,14 @@
+# ask user for year
+year = int(input("Enter a year: "))
+
+# evaluate if it's a leap year and print answer
+if year % 4== 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(year, "is a leap year.")
+        else:
+            print(year, "is not a leap year.")
+    else:
+        print(year, "is a leap year.")
+else:
+    print(year, "is not a leap year.")
