f745eebb60e9a5740f3d6f0f4354f110bd39060c
CarmaNayeli <caitlyn.c.nayeli@gmail.com>
2025-08-23 17:46:15 +0300

    Delete Question6.py

--- Question6.py
+++
@@ -1,8 +0,0 @@
-#import library to access random numbers
-import random
-
-# print 3-digit code
-print("3-digit code: " + str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)))
-
-# print 4-digit code
-print("4-digit code: " + str(random.randint(1, 6)) + str(random.randint(1, 6)) + str(random.randint(1, 6)) + str(random.randint(1, 6)))
