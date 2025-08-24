23a40a2dc493d816d49b4216f9f3e270f4f04b8c
CarmaNayeli <caitlyn.c.nayeli@gmail.com>
2025-08-24 15:32:13 +0300

    Update Question6.py

--- Module4/Question6.py
+++ Module4/Question6.py
@@ -3,11 +3,12 @@ import random
 
 # set the total number of random points (N) to user input
 N = int(input("Enter the number of random points to generate: "))
-# set the number of random points that fall within the unit circle to 0
+# initialize the number of random points that fall within the unit circle to 0
 n = 0
 
-# for each number from 1 until the total number of given points N, generate a random point
-for number in range(N):
+# for each number from 0 until the total number of given points N, generate a random point
+z = range(0, N)
+for i in z:
     x = random.uniform(-1, 1)
     y = random.uniform(-1, 1)
 
@@ -15,10 +16,7 @@ for number in range(N):
     if ((x**2) + (y**2)) < 1:
         # if inside, increment n
         n += 1
-
 # calculate approximation of pi
-approxPI = (4*n)/N
+print("Approximation of pi:", 4*n/N)
 
-#print answer
-print(f"Approximation of pi based on {N:.0f} points: ")
-print(approxPI)
+#it took so long to format this correctly because the question does not specify and I hate this
